c19a1ab0aa54783e66f0f5f542b2a6b7a006e763
Chris Malone <surefoot@gmail.com>
2014-12-31 14:05:01 -0500

    adding python 2.7.9 altinstall for centos

---
+++ scripts/centos-python2.7.9-altinstall.sh
@@ -0,0 +1,28 @@
+#!/bin/env bash
+# http://eemyop.blogspot.com/2013/04/gae-on-rhel-63-building-python-27-from.html
+
+set -e
+
+yum install -y zlib-dev openssl-devel sqlite-devel bzip2-devel xz-libs tk-devel gdbm-devel ncurses-devel readline-devel xz
+
+cd /tmp
+
+wget -q http://www.python.org/ftp/python/2.7.9/Python-2.7.9.tar.xz
+
+xz -d Python-2.7.9.tar.xz
+tar -xf Python-2.7.9.tar
+cd Python-2.7.9
+./configure --prefix=/usr/local
+make
+make altinstall
+
+# trying out builtin pip on 2.7.9
+
+/usr/local/bin/python2.7 -m ensurepip
+
+/usr/local/bin/python2.7 -m pip install virtualenv
+
+/usr/local/bin/python2.7 -m pip install virtualenvwrapper
+
+yum clean all
+rm -rf /tmp/Python-2.7.9
