761c91a8db62215e37e56b3df61401f2b07cceea
Chris Malone <surefoot@gmail.com>
2015-01-12 17:38:10 -0500

    added develop and .virtualenvs directories to vagrant home

--- scripts/centos-python2.7.9-altinstall.sh
+++ scripts/centos-python2.7.9-altinstall.sh
@@ -26,9 +26,12 @@ make altinstall
 
 rm -rf /tmp/Python-2.7.9*
 
+mkdir /home/vagrant/{develop,.virtualenvs}
+chown vagrant:vagrant /home/vagrant/{develop,.virtualenvs}
+
 cat << EOF >> /home/vagrant/.bash_profile
 export WORKON_HOME=\$HOME/.virtualenvs
-export PROJECT_HOME=\$HOME/MyProjects
+export PROJECT_HOME=\$HOME/develop
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2.7
 source /usr/local/bin/virtualenvwrapper.sh
 EOF
