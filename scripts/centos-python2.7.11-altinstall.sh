9dcf38911a48e9638b3fa682d8327ffb84f1ccd9
Chris Malone <surefoot@gmail.com>
2016-01-22 02:32:53 -0500

    change install directory to vagrant home

--- scripts/centos-python2.7.11-altinstall.sh
+++ scripts/centos-python2.7.11-altinstall.sh
@@ -12,23 +12,24 @@ wget -q https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tar.xz
 xz -d Python-2.7.11.tar.xz
 tar -xf Python-2.7.11.tar
 cd Python-2.7.11
-./configure --prefix=/usr/local
+./configure --prefix=/home/vagrant/.local
 make
 make altinstall
 
-/usr/local/bin/python2.7 -m ensurepip --upgrade
-/usr/local/bin/python2.7 -m pip install --upgrade pip
-/usr/local/bin/python2.7 -m pip install virtualenvwrapper
+/home/vagrant/.local/bin/python2.7 -m ensurepip --upgrade
+/home/vagrant/.local/bin/python2.7 -m pip install --upgrade pip
+/home/vagrant/.local/bin/python2.7 -m pip install virtualenvwrapper
 
 rm -rf /tmp/Python-2.7.11*
 
 mkdir /home/vagrant/{develop,.virtualenvs}
-chown vagrant:vagrant /home/vagrant/{develop,.virtualenvs}
+chown -R vagrant:vagrant /home/vagrant/{develop,.virtualenvs,.local}
 
+sed -i 's\bin\.local/bin\' /home/vagrant/.bash_profile
 cat << EOF >> /home/vagrant/.bash_profile
 export WORKON_HOME=\$HOME/.virtualenvs
 export PROJECT_HOME=\$HOME/develop
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2.7
-source /usr/local/bin/virtualenvwrapper.sh
+export VIRTUALENVWRAPPER_PYTHON=\$HOME/.local/bin/python2.7
+source \$HOME/.local/bin/virtualenvwrapper.sh
 EOF
 
