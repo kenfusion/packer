b0177be32c30a6605d87433d6b434924826f3246
Chris Malone <surefoot@gmail.com>
2016-10-22 11:44:42 -0400

    implement pyenv-install.sh

--- scripts/pyenv-install.sh
+++ scripts/pyenv-install.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 set -e
-export PATH="$HOME/.pyenv/bin:$PATH"
+export PATH="$HOME"/.pyenv/bin:"$PATH"
 
-sudo yum install -y openssl-devel sqlite-devel bzip2-devel xz-libs tk-devel gdbm-devel ncurses-devel readline-devel xz git
+#Install dependencies
+sudo yum install -y openssl-devel sqlite-devel bzip2-devel xz-libs tk-devel gdbm-devel ncurses-devel readline-devel xz git patch
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer -o /tmp/pyenv-installer
 
-curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer -o pyenv-installer
-bash pyenv-installer
+#Install pyenv
+bash /tmp/pyenv-installer
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
+#Install python 2 and 3
+pyenv install 2.7.12
+pyenv install 3.5.2
+
+#Set up shell
 cat << 'EOF' > "$HOME"/.pyenvrc
-export PATH="$HOME/.pyenv/bin:$PATH"
+export PATH="$HOME"/.pyenv/bin:"$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 EOF
 
-cat << EOF >> .bash_profile
+cat << EOF >> "$HOME"/.bash_profile
 if [ -f ~/.pyenvrc ]; then
         . ~/.pyenvrc
 fi
