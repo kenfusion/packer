2eee054305b7acb3f07ba31bbc6fa8fc0b75306b
Chris Malone <surefoot@gmail.com>
2017-02-19 20:51:55 -0500

    remove centos6-32bit add Centos7

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sed -i '/^GSSAPI/d' /etc/ssh/sshd_config  
+sed -i '/^GSSAPI/d' /etc/ssh/sshd_config
 sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
-sed -i '/^HWADDR\|^UUID/d' /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i '/^HWADDR\|^UUID/d' /etc/sysconfig/network-scripts/ifcfg-e*
 rm /etc/udev/rules.d/70-persistent-net.rules
 rm /lib/udev/rules.d/75-persistent-net-generator.rules
 ln -fs /dev/null /lib/udev/rules.d/75-persistent-net-generator.rules
